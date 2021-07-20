6e78a84c084a31bad1836f9ff763ceb242eabcea
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-07-20 11:19:50 +0100

    Timing single round-trip time

--- Tests/small-tests/communication.c
+++ Tests/small-tests/communication.c
@@ -3,7 +3,7 @@
 #define SECOND_VAL            (0xCAFEBABE)
 
 int main(void) {
-  int secure_world;
+  int secure_world, start_time, end_time;
   asm volatile(
     "csrr %0, 0xFC0" //CSR indicating whether a core is in the secure world or not
     : "=r" (secure_world) //output
@@ -12,9 +12,21 @@ int main(void) {
   );
   unsigned int* ptr = (unsigned int*) COMMUNICATION_ADDRESS;
   if(secure_world == 0) {
+    asm volatile(
+      "rdcycle %0"
+      : "=r" (start_time) //output
+      : //input
+      : //clobbered
+    );
     *ptr = FIRST_VAL;
     while(*ptr != SECOND_VAL);
-    return *ptr;
+    asm volatile(
+      "rdcycle %0"
+      : "=r" (end_time) //output
+      : //input
+      : //clobbered
+    );
+    return end_time - start_time;
   } else {
     while(*ptr != FIRST_VAL);
     *ptr = SECOND_VAL;
