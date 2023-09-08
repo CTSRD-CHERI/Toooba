aa440bde6d322116e4e8230032c7d6383df3d492
Peter Rugg <pdr32@cam.ac.uk>
2023-09-08 13:12:48 +0100

    Restrict to three cap pipes

--- builds/Resources/Include_Common.mk
+++ builds/Resources/Include_Common.mk
@@ -94,7 +94,7 @@ BSC_COMPILATION_FLAGS += \
 	-D RAS_HIT_TRACING \
 	-D TSO_MM \
 	-D NO_SPEC_TRAINING -D NO_SPEC_REDIRECT -D NO_SPEC_STRAIGHT_PATH -D SPEC_RSB_FIXUP -D MELTDOWN_CF \
-	-D MAX_CAP_PIPES=4 \
+	-D MAX_CAP_PIPES=3 \
 	-keep-fires -aggressive-conditions -no-warn-action-shadowing -check-assert \
 	-suppress-warnings G0020 -steps-max-intervals 10000000   \
 	-steps-warn-interval 1000000 \
